4ce69575c6d693d018f1a5cb6bc8ac7c4f5028e9
3bker <44803452+3bker@users.noreply.github.com>
2018-11-10 15:18:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+ const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
@@ -6,8 +6,17 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("510213741204340751")
 setInterval(function() {
-channel.send(`3bker fo2rerererererererererererere`);
+channel.send(`3bker up 3bker up 3bker up 3bker up 3bker up`);
 }, 30)
 })
 
-client.login(process.env.BOT_TOKEN);
+
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "5s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+client.login(process.env.BOT_TOKEN); 
